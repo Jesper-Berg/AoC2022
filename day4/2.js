56afb4a8e2e6c6cdfe4244c39c6705f77075ead2
Jesper Berg <jesper.berg@cygni.se>
2022-12-04 11:44:17 +0100

    realized I didnt need the old conditions

--- day4/2.js
+++ day4/2.js
@@ -14,8 +14,6 @@ const overlapCount = input.reduce(
   (count, pair) =>
     count +
     !!(
-      (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1]) ||
-      (pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1]) ||
       (pair[0][0] >= pair[1][0] && pair[0][0] <= pair[1][1]) ||
       (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][0])
     ),
