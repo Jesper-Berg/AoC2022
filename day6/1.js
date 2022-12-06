9894e984d4fb90cd631967ef98601795bb5d301f
Jesper Berg <jesper.berg@cygni.se>
2022-12-06 15:51:06 +0100

    Good solution for day 6

---
+++ day6/1.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+
+const input = fs.readFileSync('./input.txt').toString().split('');
+
+const hasDuplicates = (buffer) => new Set(buffer).size !== buffer.length;
+
+const buffer = [];
+
+for (const [i, char] of input.entries()) {
+  if (buffer.length === 4) {
+    if (!hasDuplicates(buffer)) {
+      console.log(i);
+      break;
+    }
+    buffer.shift();
+  }
+  buffer.push(char);
+}
